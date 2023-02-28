033cd21f65f989be14f3af1d2c2fa30c6e1ae1ee
ZmeykaTatiana <zacharovanaja@mail.ru>
2023-02-28 21:27:28 +0300

    update validation of creating new person

--- src/main/java/by/zmeyka/SpringMVC/Model/Person.java
+++ src/main/java/by/zmeyka/SpringMVC/Model/Person.java
@@ -1,12 +1,13 @@
 package by.zmeyka.SpringMVC.Model;
 
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
+
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
+
 
 @Data
 @NoArgsConstructor
@@ -14,10 +15,10 @@ import lombok.NonNull;
 public class Person {
     private int id;
 
-  @NotNull(message="can't be empty")
+  @NotEmpty(message="can't be empty")
   @Size(min=2, max=35, message="Name should be shorter.try again.")
     private String name;
-    @NotNull(message="can't be empty")
+    @NotEmpty(message="can't be empty")
     @Size(min=2, max=35, message="Name should be shorter.try again.")
     private String surname;
 
